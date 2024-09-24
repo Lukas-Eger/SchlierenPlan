7a548d60fb08702ceca3dd3b41d57e1e188f789c
Lukas-Eger <lukas_eger@web.de>
2024-09-24 13:38:05 +0200

    soll ich aus datei die paramter nehmen oder lokal frage hinzugefügt

--- py/main.py
+++ py/main.py
@@ -20,43 +20,51 @@ img=cv.imread('../img/V2/ancycamsy_5mm_.bmp',cv.IMREAD_GRAYSCALE)
 imgBack=cv.imread('../img/V2/bildohne5mm.bmp',cv.IMREAD_GRAYSCALE)
 
 #einlesne der Paremter
-'''
-parameter = {
-    "bgImgAvailable" : True,
-    
-    "doShadingCorrection": True,
-    
-    "cropImage": True,
-    
-    "setupContrast": True,
-    "alpha": 1.8,   #lower contrast: alpha < 1, higher contrast alpha > 1 
-    "beta": -30,    #brightness -127 < beta < 127
-    
-    "edgeEnhancement": True,
-    "laplacianFilteredImage": False,
-    "ddepthLaplacian": cv.CV_8U,    #CV_8U, CV_16S, CV_32F, CV_64F
-    "kernelSizeLaplacian": 7,
-    "pillowSharpendImage": True,
-    "factorSharpness": 21.0,
-    "sobeFilteredImage": False,
-    "kernelSizeSobelFilter": 7,
-    "ddepthSobel": cv.CV_8U,        #CV_8U, CV_16S, CV_32F, CV_64F
-    
-    "deblureImage": True,
-    "meanFilteredImage": False,
-    "kernelSizeMeanFilter": 5,
-    "gaussianFilteredImage": False,
-    "kernelSizeGaussianFilter": 31,
-    "standardDeviation": 0,     #calculated from kernelSize
-    "medianFilteredImage": True,
-    "kernelSizeMedianFilter":9,
-    "bilateralFilteredImage": False,
-    "bilateralFilterDiameter": 31,   #diameter of each pixel neighborhood that is used during filtering
-    "bilateralFilterSigma": 250  #<10: no effect >150: huge effect on outcome
-}
-'''
-with open('config.json', 'r') as cf:
-  parameter = json.load(cf)
+localuse= input("möchten sie die lokalen Parameter oder die aus einer Datei verwenden(l/d)")
+if(localuse=="l"):
+
+    parameter = {
+        "bgImgAvailable" : True,
+        
+        "doShadingCorrection": True,
+        
+        "cropImage": True,
+        "x_start":0,
+        "y_start":0,
+        "x_end":0,
+        "y_end":0,
+       
+        
+        
+        "setupContrast": True,
+        "alpha": 1.8,   #lower contrast: alpha < 1, higher contrast alpha > 1 
+        "beta": -30,    #brightness -127 < beta < 127
+        
+        "edgeEnhancement": True,
+        "laplacianFilteredImage": False,
+        "ddepthLaplacian": cv.CV_8U,    #CV_8U, CV_16S, CV_32F, CV_64F
+        "kernelSizeLaplacian": 7,
+        "pillowSharpendImage": True,
+        "factorSharpness": 21.0,
+        "sobeFilteredImage": False,
+        "kernelSizeSobelFilter": 7,
+        "ddepthSobel": cv.CV_8U,        #CV_8U, CV_16S, CV_32F, CV_64F
+        
+        "deblureImage": True,
+        "meanFilteredImage": False,
+        "kernelSizeMeanFilter": 5,
+        "gaussianFilteredImage": False,
+        "kernelSizeGaussianFilter": 31,
+        "standardDeviation": 0,     #calculated from kernelSize
+        "medianFilteredImage": True,
+        "kernelSizeMedianFilter":9,
+        "bilateralFilteredImage": False,
+        "bilateralFilterDiameter": 31,   #diameter of each pixel neighborhood that is used during filtering
+        "bilateralFilterSigma": 250  #<10: no effect >150: huge effect on outcome
+    }
+else:
+    with open('config.json', 'r') as cf:
+      parameter = json.load(cf)
 
 plt.close("all")
 #einlesen Kamerametrix
